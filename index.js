187a8d89c08b5efd0851db73ddf29fdd3ae35747
VijayPathem1 <vijaypathem122@gmail.com>
2022-10-07 13:22:48 +0530

    first

---
+++ index.js
@@ -0,0 +1,21 @@
+let course=
+{
+    languages:["JavaScript","Python","Ruby","Go"],
+    showtitle()
+    {
+        let msg=this.languages
+        this.languages.forEach(function(items){
+            console.log(items);
+        })
+        console.log(msg)
+    },
+    var :isChallenging=true,
+    var :isRewarding=true,
+    var :value=7,
+    var: isFun=true
+}
+course.showtitle()
+console.log(isChallenging);
+console.log(isRewarding);
+console.log(value);
+console.log(isFun);
